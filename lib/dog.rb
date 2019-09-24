0a823bca55271ba9d3570ae28f2aeb52104b695a
Srikalyan Megati <kalmegati@gmail.com>
2019-09-24 15:53:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def naming(dog_name)
+  def name=(dog_name)
     @this_dogs_name = dog_name
   end
  
@@ -9,6 +9,6 @@ class Dog
 end
 
 lassie = Dog.new
-lassie.naming("Lassie")
+lassie.name = "Lassie"
 
 puts lassie.name
